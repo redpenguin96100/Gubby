6174d33c3d0d7c2159bfe4998aa58490ef1471c4
redpenguin96100 <260103249+redpenguin96100@users.noreply.github.com>
2026-02-09 17:02:34 -0600

    Update aiwrpi.sh

--- aiw/scripts/aiwrpi.sh
+++ aiw/scripts/aiwrpi.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-echo '
-   _____  .__  .__     .___           __      __       .__  .__
-  /  _  \ |  | |  |    |   | ______  /  \    /  \ ____ |  | |  |
- /  /_\  \|  | |  |    |   |/  ___/  \   \/\/   // __ \|  | |  |
-/    |    \  |_|  |__  |   |\___ \    \        /\  ___/|  |_|  |__
-\____|__  /____/____/  |___/____ >     \__/\  /  \___  >____/____/
-        \/                      \/          \/       \/            '
+cat << 'EOF'
+   ____       _     _             _       _                   
+  / ___|_   _| |__ | |__  _   _  (_)___  | |__   ___ _ __ ___ 
+ | |  _| | | | '_ \| '_ \| | | | | / __| | '_ \ / _ \ '__/ _ \
+ | |_| | |_| | |_) | |_) | |_| | | \__ \ | | | |  __/ | |  __/
+  \____|\__,_|_.__/|_.__/ \__, | |_|___/ |_| |_|\___|_|  \___|
+                          |___/                               
+  _           __ _                                      _                          
+ | |_ ___    / _(_)_  __  _   _ _ __   _ __   __ _  ___| | ____ _  __ _  ___  ___  
+ | __/ _ \  | |_| \ \/ / | | | | |__| | |_ \ / _` |/ __| |/ / _` |/ _` |/ _ \/ __| 
+ | || (_) | |  _| |>  <  | |_| | |    | |_) | (_| | (__|   < (_| | (_| |  __/\__ \ 
+  \__\___/  |_| |_/_/\_\  \__,_|_|    | .__/ \__,_|\___|_|\_\__,_|\__, |\___||___/ 
+                                      |_|                         |___/            
+EOF
 # Start Of The Script:
 echo -en '\n'
 echo -e "Just A Small Script For Updating/Upgrading And Fixing Packages On Linux \e[7m(Raspberry Pi OS)\e[27m"
@@ -13,50 +20,17 @@ echo 'Version 2.0'
 echo -en '\n'
 echo -en '\n'
 
-function error {
-  echo -e "\\e[91m$1\\e[39m"
-  exit 1
-}
-#Update Check:
-echo -e "\e[31mChecking for updates...\e[0m"
-cd $HOME/All-is-well
-localhash="$(git rev-parse HEAD)"
-latesthash="$(git ls-remote https://github.com/spectrumgamer75/All-is-well HEAD | awk '{print $1}')"
-if [ "$localhash" != "$latesthash" ] && [ ! -z "$latesthash" ] && [ ! -z "$localhash" ];then
-# Update (if out of date):
-echo "Out of date, updating now..."
-git clean -fd
-git reset --hard
-git pull https://github.com/spectrumgamer75/All-is-well HEAD || error '\e[31mUnable to update, please check your internet connection...\e[0m'
-cd $HOME/All-is-well/aiw
-chmod +x aiwrpi.sh
-clear
-# Update (if up to date):
-else
-  echo -e "\e[32mUp to date! \e[0m"
-fi
+
 # Prompt:
 echo "Please enter your password, if any to start ッ"
 echo -en '\n'
 # Main:
 sudo apt update --fix-missing || error '\e[31mUnable to update, please check your internet connection...\e[0m' && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y
 echo -en && sudo apt install -f && sudo dpkg --configure -a
-#Comment 'clear' below to stop clearing the output:
+
 clear -x
 # Ending:
-echo "All is well now (if everything went correctly) press x to exit ◠‿◠"
-count=0
-while : ; do
-read -n 1 k <&1
-if [[ $k = x ]] ; then
-printf "\nQuitting from the script\n"
-break
-else
-((count=$count+1))
-printf "\nWrong key pressed for $count times\n"
-echo "All is well now press x to exit :)"
-fi
-done
+echo gubby has fixed ur package
 
 # About this Script:
 # ALL IS WELL is a bash script that allows users to easily update and upgrade their repositories and packages on linux, BSD, And More. It will also fix any broken packages and dependencies, if possible. Forget about typing all update/upgrade commands  manually, when ALL IS WELL!
@@ -65,4 +39,5 @@ done
 # SCRIPT AUTHOR: SPECTRUMGAMER75 (^̮^)                #
 # www.github.com/spectrumgamer75                     #
 # Incase of any errors kindly let me know on GitHub. #
+#Gubbyed by @redpenguin96100	  		                 #
 ######################################################
