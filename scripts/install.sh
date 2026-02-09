c4f53ad038b55c5f6ba95c2d416b78510c6a7f7b
redpenguin96100 <260103249+redpenguin96100@users.noreply.github.com>
2026-02-09 17:31:12 -0600

    Update install.sh for Gubby installation process
    not done

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,12 @@
-#does stuff
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+echo "Gubby installation started!"
+
+git clone https://github.com/redpenguin96100/Gubby.git
+cd Gubby
+
+
+echo "Gubby installation finished!"
