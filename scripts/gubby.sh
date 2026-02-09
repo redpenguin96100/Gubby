c3b336a1b6753a9b9f391d4d90f8bf704795f6a1
redpenguin96100 <260103249+redpenguin96100@users.noreply.github.com>
2026-02-09 17:35:26 -0600

    Update gubby.sh

--- scripts/gubby.sh
+++ scripts/gubby.sh
@@ -1,12 +1,22 @@
 echo get gubbyed
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-"$SCRIPT_DIR/aiwrpi2.sh"
+cd 
+cd gubby
+cd aiw 
+cd scripts
+./aiwrpi2.sh
+
+#pihole updating
 sudo pihole -up
 sudo pihole -g
 sudo pihole -r
+#fastfetch
 fastfetch
 sleep 10
 clear
+cd ..
+cd ..
 cat "$SCRIPT_DIR/gubby_art.txt"
+#moo
 apt moo
