44d40779cffe169c5c72fd22b8a49858aa1a9232
redpenguin96100 <260103249+redpenguin96100@users.noreply.github.com>
2026-02-09 18:15:44 -0600

    make install.sh do the install things

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
+set -eu
+# F
+failure_message() {
+    echo "❌ Error: Installation failed. idk what broke you figure it out."
+}
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
+trap failure_message ERR
 
+TARGET_DIR="$HOME/Gubby"
 echo "Gubby installation started!"
 
-git clone https://github.com/redpenguin96100/Gubby.git
-cd Gubby
-
-
+if ! command -v git &> /dev/null; then
+    echo "git not found. Installing..."
+    sudo apt update && sudo apt install -y git
+fi
+# 2. Only clone if the directory doesn't exist
+if [ ! -d "$TARGET_DIR" ]; then
+    git clone https://github.com/redpenguin96100/Gubby.git "$TARGET_DIR"
+else
+    echo "Gubby folder already exists, trying to update"
+    git -C "$TARGET_DIR" pull 
+fi
+find "$TARGET_DIR" -type f -name "*.sh" -exec chmod +x {} +
+if ! grep -q "alias gubby=" ~/.bash_aliases; then
+    echo 'alias gubby="$HOME/Gubby/scripts/gubby.sh"' >> ~/.bash_aliases
+fi
 echo "Gubby installation finished!"
+echo "Run source .bash_aliases or open a new terminal"
+echo " then just type gubby in a terminal"
